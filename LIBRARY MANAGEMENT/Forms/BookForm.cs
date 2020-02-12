3366223d9913f7cb4145fe7b0940950041a00be2
abdullayevahilal <HilalMa@code.edu.az>
2020-02-12 20:54:32 +0400

    Book CRUD end

--- LIBRARY MANAGEMENT/Forms/BookForm.cs
+++ LIBRARY MANAGEMENT/Forms/BookForm.cs
@@ -54,41 +54,7 @@ namespace LIBRARY_MANAGEMENT.Forms
                 };
                 CmbGenre.Items.Add(combobox);
             }
-        }
-        #region 
-        //Create and Read Method
-        //private void BtnBookAdd_Click_1(object sender, EventArgs e)
-        //{
-        //    CmbCombobox select = CmbGenre.SelectedItem as CmbCombobox;
-        //    if (!string.IsNullOrEmpty(TxtBookName.Text) ||
-        //     !string.IsNullOrEmpty(TxtAuthor.Text) ||
-        //     !string.IsNullOrEmpty(CmbGenre.Text) ||
-        //     !string.IsNullOrEmpty(TxtRentalPrice.Text) ||
-        //     !string.IsNullOrEmpty(TxtCount.Text))
-        //    {
-
-        //        Book book = new Book//add book to database
-        //        {
-        //            Name = TxtBookName.Text,
-        //            Author = TxtAuthor.Text,
-        //            GenreId = select.Id,
-        //            RentalPrice = Convert.ToDouble(TxtRentalPrice.Text),
-        //            Count = Convert.ToInt32(TxtCount.Text),
-        //        };
-
-        //        _context.Books.Add(book);
-        //        _context.SaveChanges();
-        //        Clear();
-
-
-        //    }
-        //    else
-        //    {
-        //        MessageBox.Show("Add,please");
-        //    }
-        //    FillBooks();
-
-        //}
+        }            
         private void Clear()
         {
             TxtBookName.Clear();
@@ -97,89 +63,13 @@ namespace LIBRARY_MANAGEMENT.Forms
             TxtRentalPrice.Clear();
             CmbGenre.SelectedItem = null;
         }
-        #endregion
-        #region
-        //Search Method
-        //private void BtnSearch_Click_1(object sender, EventArgs e)
-        //{
-        //    CmbCombobox select = CmbGenre.SelectedItem as CmbCombobox;
-        //    if (string.IsNullOrEmpty(TxtBookName.Text) &&
-        //     string.IsNullOrEmpty(TxtAuthor.Text) &&
-        //     CmbGenre.SelectedItem != null)
-        //    {
-        //        MessageBox.Show("Please fill at least one field");
-        //        return;
-        //    }
-        //    var book = _context.Books
-        //                            .Where(b => (TxtBookName.Text != string.Empty ? b.Name.Contains(TxtBookName.Text) : false))
-
-
-        //                            .OrderBy(b => b.Name)
-        //                            .ToList();
-
-        //    DgvAddBooks.Rows.Clear();
-        //    foreach (var item in book)
-        //    {
-        //        DgvAddBooks.Rows.Add(item.Id,
-        //                             item.Name,
-        //                             item.Author,
-        //                             item.Genre.Name,
-        //                             item.RentalPrice,
-        //                             item.Count);
-        //    }
-            
-        //}
-        #endregion
-        //private void BookForm_Load_1(object sender, EventArgs e)
-        //{
-        //    FillBooks();
-        //}
-        //Delete Method
-        //private void BtnDelete_Click_1(object sender, EventArgs e)
-        //{
-        //    _context.Books.Remove(_book);
-        //    _context.SaveChanges();
-        //    FillBooks();
-        //}    
-        //Update
-        //private void BtnUpdate_Click_1(object sender, EventArgs e)
-        //{
-        //    CmbCombobox select = CmbGenre.SelectedItem as CmbCombobox;
-        //    Book book = _context.Books.Find(_book.Id);
-
-        //    book.Name = TxtBookName.Text;
-        //    book.Author = TxtAuthor.Text;
-        //    book.Count = Convert.ToInt32(TxtCount.Text);
-        //    book.RentalPrice = Convert.ToDouble(TxtRentalPrice.Text);
-        //    book.GenreId = select.Id;
-
-        //    _context.SaveChanges();
-        //    FillBooks();
-        //}
-        //private void DgvAddBooks_RowHeaderMouseClick_1(object sender, DataGridViewCellMouseEventArgs e)
-        //{
-        //    CmbCombobox select = CmbGenre.SelectedItem as CmbCombobox;
-
-        //    TxtBookName.Text = _book.Name;
-        //    TxtAuthor.Text = _book.Author;
-        //    TxtCount.Text = _book.Count.ToString();
-        //    TxtRentalPrice.Text = _book.RentalPrice.ToString();
-        //    Genre genre = _context.Genres.Find(_book.GenreId);
-        //    CmbGenre.SelectedItem = new CmbCombobox
-        //    {
-        //        Id = genre.Id,
-        //        Name = genre.Name
-        //    };
-
-        //    CmbGenre.Text = genre.Name;
-        //}
-
+        
         private void DgvAddBooks_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
         {
             _id = (int)DgvAddBooks.Rows[e.RowIndex].Cells[0].Value;
             _book = _context.Books.Find(_id);
         }
-
+        //Create and Read
         private void BtnBookAdd_Click(object sender, EventArgs e)
         {
             CmbCombobox select = CmbGenre.SelectedItem as CmbCombobox;
@@ -212,7 +102,7 @@ namespace LIBRARY_MANAGEMENT.Forms
             FillBooks();
 
         }
-
+        //Search Method
         private void BtnSearch_Click(object sender, EventArgs e)
         {
             CmbCombobox select = CmbGenre.SelectedItem as CmbCombobox;
@@ -240,9 +130,10 @@ namespace LIBRARY_MANAGEMENT.Forms
                                      item.RentalPrice,
                                      item.Count);
             }
+            Clear();
 
         }
-
+        //Update Method
         private void BtnUpdate_Click(object sender, EventArgs e)
         {
             CmbCombobox select = CmbGenre.SelectedItem as CmbCombobox;
@@ -256,8 +147,9 @@ namespace LIBRARY_MANAGEMENT.Forms
 
             _context.SaveChanges();
             FillBooks();
+            Clear();
         }
-
+        //Delete Method
         private void BtnDelete_Click(object sender, EventArgs e)
         {
             _context.Books.Remove(_book);
